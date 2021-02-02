2050a1479861f37011380995d930d092b9e5d151
yuchao.xie <yuchao.xie@alo7.com>
2021-02-02 17:18:45 +0800

    feat(QRCode): 添加隐藏二维码功能

--- webview/src/component/QRCode/QRCodeArea.tsx
+++ webview/src/component/QRCode/QRCodeArea.tsx
@@ -1,6 +1,6 @@
-import React, { memo, useCallback } from 'react'
+import React, { memo, useCallback, useState } from 'react'
 import { toCanvas } from 'qrcode'
-import { root } from './QRCodeArea.css'
+import { root, list, hidden } from './QRCodeArea.css'
 import { DefaultQRCodeText } from '../../config'
 import { PostMessage } from '../../message'
 
@@ -11,14 +11,22 @@ interface IProps {
 export const QRCodeArea = memo(function QRCodeArea({
     text
 }: Readonly<IProps>): JSX.Element {
+    const [showQRCode, setShowQRCode] = useState(true)
     const onClickSave = useCallback(() => {
         PostMessage({ type: 'SaveQRCode', data: text })
     }, [text])
+    const onClickCheckBox = useCallback(() => {
+        setShowQRCode(v => !v)
+    }, [])
     return (<div className={root}>
         <canvas
+            className={showQRCode ? '' : hidden}
             ref={(ref): Promise<void> | null =>
                 ref && toCanvas(ref, text || DefaultQRCodeText)}
         />
-        <button disabled={!text} onClick={onClickSave}>保存二维码</button>
+        <div className={list}>
+            <button className={showQRCode ? '' : hidden} disabled={!text} onClick={onClickSave}>保存二维码</button>
+            <button onClick={onClickCheckBox}>{showQRCode ? '隐藏二维码' : '显示二维码'}</button>
+        </div>
     </div>)
 })
